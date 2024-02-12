043582f14b55322a41a75276caee06ef9e86c507
kimkyungsub <cyon256@icloud.com>
2024-02-12 15:58:48 +0900

    update

--- main.go
+++ main.go
@@ -10,9 +10,16 @@ func main() {
 
 	rl.InitWindow(screenWidth, screenHeight, "raylib [text] example - ttf loading")
 
-	msg := "안녕"
-	font := rl.LoadFontEx("fonts/Kart_gothic_medium.ttf", 96, []rune(msg))
+	hello := Header{
+		Msg:      "안녕",
+		Position: rl.NewVector2(0, 0),
+		Spacing:  0,
+		Color:    rl.Black,
+	}
 
+	msg := "안녕"
+
+	font := rl.LoadFontEx("fonts/Kart_gothic_medium.ttf", 96, []rune(msg))
 	fontSize := font.BaseSize
 	fontPosition := rl.NewVector2(float32(screenWidth)/2-rl.MeasureTextEx(font, msg, float32(fontSize), 0.0).X/2, 0)
 
@@ -29,4 +36,21 @@ func main() {
 
 	rl.UnloadFont(font) // Font unloading
 	rl.CloseWindow()
+}
+
+type Header struct {
+	Font     rl.Font
+	Msg      string
+	Position rl.Vector2
+	Size     float32
+	Spacing  float32
+	Color    rl.Color
+}
+
+func (h *Header) CenterX(screenWidth float32) {
+	h.Position.X = screenWidth/2 - rl.MeasureTextEx(h.Font, h.Msg, h.Size, h.Spacing).X/2
+}
+
+func (h *Header) setFont(filename string) {
+	h.Font = rl.LoadFontEx(filename, 96, []rune(h.Msg))
 }
